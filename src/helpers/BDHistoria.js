88fe6b520fc1b9d120e89b9cbef00b4c4f95f8a8
Manupatino16 <manuelap1604@gmail.com>
2022-09-25 20:57:01 -0500

    spotify

---
+++ src/helpers/BDHistoria.js
@@ -0,0 +1,12 @@
+let artistas=[
+    {foto:'https://firebasestorage.googleapis.com/v0/b/spotify-39cf4.appspot.com/o/cantante1.jpg?alt=media&token=c5535d64-3966-438b-8b76-05614826e96b',historia},
+    {foto:'https://firebasestorage.googleapis.com/v0/b/spotify-39cf4.appspot.com/o/cantante2.jpg?alt=media&token=072b926f-e313-4bdd-834e-3562c1075067',historia},
+    {foto:'https://firebasestorage.googleapis.com/v0/b/spotify-39cf4.appspot.com/o/cantante3.png?alt=media&token=c51e71a6-282f-4174-9081-3015aac203a4',historia},
+    {foto:'https://firebasestorage.googleapis.com/v0/b/spotify-39cf4.appspot.com/o/cantante4.jpg?alt=media&token=685a9c65-1ebc-49c3-90bf-c47cd86ceb47',historia},
+    {foto:'https://firebasestorage.googleapis.com/v0/b/spotify-39cf4.appspot.com/o/cantante5.png?alt=media&token=e52d1494-54af-4bce-a896-cf3de4ab0034',historia},
+    {foto:'https://firebasestorage.googleapis.com/v0/b/spotify-39cf4.appspot.com/o/cantante6.jpg?alt=media&token=cb92ba2f-78e4-45b5-ae76-e39cbb28467e',historia},
+    {foto:'https://firebasestorage.googleapis.com/v0/b/spotify-39cf4.appspot.com/o/cantante7.jpg?alt=media&token=7bda907f-5c95-4910-a7e0-469498017b03',historia},
+    {foto:'https://firebasestorage.googleapis.com/v0/b/spotify-39cf4.appspot.com/o/cantante8.jpg?alt=media&token=24409722-f09b-47a6-b800-efd9fe073aca',historia},
+    {foto:'https://firebasestorage.googleapis.com/v0/b/spotify-39cf4.appspot.com/o/cantante9.jpg?alt=media&token=db2eddc2-7fdb-47f6-8932-b4c6daf6a72e',historia},
+    {foto:'https://firebasestorage.googleapis.com/v0/b/spotify-39cf4.appspot.com/o/cantante10.jpeg?alt=media&token=3a1cb7f3-9f19-4892-b91f-f723a5bdbf19',historia}
+]
---
+++ src/helpers/constantesGET.js
@@ -0,0 +1,6 @@
+const TOKEN="Bearer BQCsPqtOHT1SubfHvwYZSPMjM_v7lMIQfzhsOzMKKpWBLV2UD9Kvv-2ZLc4fFsz5EKMnpDsFp_DST_oofv6iMtvd0M5f3nWo2mKU3x4CAQbx9tVfpjcMizmrpnhbffdlfaTe2SUiqpKgz-Ok_SWOqOxfJqKQZwOzaiwg7ks7EG0mFPNaU1ibQBG-O7u9TNsOUeE"
+
+    export const PETICION={
+        method:"GET",
+        headers:{Authorization:TOKEN},
+    }
