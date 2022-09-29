7ac0da67d7c198954d27655ffb54d99a0cd0716e
Manupatino16 <manuelap1604@gmail.com>
2022-09-29 12:31:11 -0500

    spotify

--- src/helpers/constantesGET.js
+++ src/helpers/constantesGET.js
@@ -1,4 +1,4 @@
-const TOKEN="Bearer BQCsPqtOHT1SubfHvwYZSPMjM_v7lMIQfzhsOzMKKpWBLV2UD9Kvv-2ZLc4fFsz5EKMnpDsFp_DST_oofv6iMtvd0M5f3nWo2mKU3x4CAQbx9tVfpjcMizmrpnhbffdlfaTe2SUiqpKgz-Ok_SWOqOxfJqKQZwOzaiwg7ks7EG0mFPNaU1ibQBG-O7u9TNsOUeE"
+const TOKEN="Bearer BQBzPMw4NWTHCcvjzCkwjcc3lY66NNyfB2jJ94NlzGqQNq2OnDixqt0ngY0mtAybjx1suuavmlpwVSJW2Vsn0Ta_E4zOmvVRT9sbiztgrLN9cc1Z5594IxuZdUFF44s_Z21cFkwBLzd9tl464vfouV2vfPl3QI_l4baiQkZnxyx7xL4oedkiQ7dNyuNLerjbWto"
 
     export const PETICION={
         method:"GET",
